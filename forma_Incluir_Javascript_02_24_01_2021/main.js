dfcd55108f4c8fd78532f8b13ff95dfc6cb57260
diego manuel guerrero torres <diegomgt11@gmail.com>
2021-01-31 18:39:13 -0500

    AJUSTES TALLER 1

---
+++ forma_Incluir_Javascript_02_24_01_2021/main.js
@@ -0,0 +1,9 @@
+let nombre  ="diego manuel guerrero torres"
+let edad = 23
+let estudiante = true
+
+const datos ={id:91526405,direccion:"calle 34 034"};
+console.log("nombre",typeof(nombre),nombre);
+console.log("edad",typeof(edad),edad);
+console.log("estudiante",typeof(estudiante),estudiante);
+console.log("informacion",typeof(datos),datos);
---
+++ pedir datos por prompt_24_01_2021/main.js
@@ -0,0 +1,10 @@
+
+
+let fecha_nacimiento = prompt("digite su fecha de nacimiento")
+//const fecha_actual = 2021
+
+// edad = fecha_actual-fecha_nacimiento
+
+// alert(`su edad es ${edad}`)
+alert("su fecha de nacimiento es:" + fecha_nacimiento);
+
